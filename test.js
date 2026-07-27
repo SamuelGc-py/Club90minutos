743dfed5363bcc9bfe414073036bf6bda2168c39
samuel <samucobaggg@gmail.com>
2026-07-27 18:08:18 -0500

    clean: remover archivos temporales de script

--- test.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client'); 
-const prisma = new PrismaClient(); 
-prisma.partido.findMany().then(p => {
-  console.log(p.map(x => ({id: x.id, jornada: x.jornada, fecha: x.fecha_hora_partido, estado: x.estado})));
-}).catch(console.error).finally(()=>prisma.$disconnect());
