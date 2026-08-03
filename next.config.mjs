322886f391466beab658474aebbaeba17f1a690e
samuel <samucobaggg@gmail.com>
2026-08-02 20:26:00 -0500

    fix: ignore eslint and ts checks to prevent OOM in Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   async headers() {
     return [
       {
